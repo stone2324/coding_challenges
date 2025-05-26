7ba7360c9965c628feb87533ae3b533e959f3b88
aidan.wong16 <aidan.wong16@education.nsw.gov.au>
2025-05-26 20:47:48 +1000

    added solution to q4

--- 04_fizzbuzz.py
+++ 04_fizzbuzz.py
@@ -18,3 +18,18 @@
 
 # def fizz_buzz() -> None:
 #     pass
+
+def fizz_buzz():
+    for number in range(1,100):
+        if number % 3 == 0:
+            if number % 5 == 0:
+                print("FizzBuzz")
+            else:
+                print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+fizz_buzz()
+
