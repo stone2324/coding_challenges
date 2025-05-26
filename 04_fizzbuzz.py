d1f374f37dd44b0fdfe5bca555adb7d4f8367d43
Kevin <kevin.wk.wong@outlook.com>
2025-05-26 20:28:52 +1000

    added q4

---
+++ 04_fizzbuzz.py
@@ -0,0 +1,20 @@
+#------------------------------
+# Problem statement
+#------------------------------
+
+# Write a function that prints the numbers from 1 to 100, but with the following rules:
+
+# For numbers that are multiples of 3, print "Fizz" instead of the number.
+
+# For numbers that are multiples of 5, print "Buzz" instead of the number.
+
+# For numbers that are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+# For all other numbers, print the number itself.
+
+#------------------------------
+# Function Signature
+#------------------------------
+
+# def fizz_buzz() -> None:
+#     pass
