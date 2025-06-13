351d28ecc7185ba1abf11a46f736afd3ff6ffdc7
aidan.wong16 <aidan.wong16@education.nsw.gov.au>
2025-06-14 09:22:43 +1000

    added q8

---
+++ 08_language_check.py
@@ -0,0 +1,45 @@
+#start: 9:05
+#end : 9:22
+
+# You received a whatsup message from an unknown number. Could it be from that girl/boy with a foreign accent you met yesterday evening?
+
+# Write a simple function to check if the string contains the word hello in different languages.
+
+# These are the languages of the possible people you met the night before:
+
+# hello - english
+# ciao - italian
+# salut - french
+# hallo - german
+# hola - spanish
+# ahoj - czech republic
+# czesc - polish
+# Notes
+
+# you can assume the input is a string.
+# to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
+# function should be case insensitive to pass the tests
+
+def validate_hello(message):
+    possible_greetings = ['hello','ciao','salut','hallo','hola','ahoj','czesc'] #list all possible languages to loop through
+    for item in possible_greetings: #loop through languages
+        if item in message.lower(): #check if the message contains a hello in any language
+            return True #if the message has hello, return true
+    return False #otherwise, return false
+
+def assertion_tests():
+    #these should be right
+    assert validate_hello('Hello there!') == True
+    assert validate_hello('Ciao') == True
+    assert validate_hello('hallo') == True
+    assert validate_hello('Ahoj') == True
+
+    #these should be false
+    assert validate_hello('hi') == False
+    assert validate_hello('helo') == False
+    assert validate_hello('ahjo') != False
+    assert validate_hello('Caio') != False
+
+def main():
+    assertion_tests()
+
