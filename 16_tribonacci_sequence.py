e2b5d8e49c4344f9cb9099e4523f393011c85124
Aidan-w16 <aidan.wong16@education.nsw.gov.au>
2025-06-20 06:49:19 +1000

    fixed assertion tests on q16

--- 16_tribonacci_sequence.py
+++ 16_tribonacci_sequence.py
@@ -33,9 +33,9 @@ def tribonacci(signature, output_digits):
 def asssertion_tests():
     assert tribonacci([1, 1, 1], 10) == [1, 1, 1, 3, 5, 9, 17, 31, 57, 105]
     assert tribonacci([0, 0, 1], 10) == [0, 0, 1, 1, 2, 4, 7, 13, 24, 44]
-    assert tribonacci([0, 1, 1], 10) == [0, 1, 1, 2, 4, 7, 13, 24, 44]
+    assert tribonacci([0, 1, 1], 10) == [0, 1, 1, 2, 4, 7, 13, 24, 44,81]
     assert tribonacci([1, 2, 3], 5) == [1, 2, 3, 6, 11]
-    assert tribonacci([0.5, -0.5, -0.5], 6) == [0.5, -0.5, -0.5, -0.5, -0.5, -0.5]
+    assert tribonacci([0.5, -0.5, -0.5], 6) == [0.5, -0.5, -0.5, -0.5, -1.5, -2.5]
     assert tribonacci([1, 2, 3], 0) == []
     assert tribonacci([1, 2, 3], 1) == [1]
 
